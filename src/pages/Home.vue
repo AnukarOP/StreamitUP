c7365898c6dba05c2b16b8cf167fcd41997ef512
Anukar <124155412+AnukarOP@users.noreply.github.com>
2023-11-04 12:00:38 +0530

    Create Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Brand from '../components/Brand.vue'
+import Button from '../components/Button.vue'
+import Navbar from '../components/Navbar.vue'
+
+const router = useRouter()
+</script>
+
+<template>
+  <Navbar>
+    <Brand />
+    <Button type="button" @click="router.push('/create')">Create room</Button>
+  </Navbar>
+
+  <main
+    className="flex-1 flex flex-col items-center justify-center space-y-10 text-center">
+    <h1 className="text-2xl md:text-3xl font-bold">StreamitUP 🚀 <br> Github.com/AnukarOP</h1>
+    <h2 className="max-w-xs text-lg md:text-xl md:max-w-md">
+      Peer-2-peer screensharing and chat, with anyone,
+      <strong>No account neded</strong>.
+    </h2>
+    <Button
+      type="button"
+      class="text-lg shadow-lg"
+      @click="router.push('/create')"
+      >Create room</Button
+    >
+  </main>
+</template>
