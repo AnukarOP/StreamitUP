cf2f0ae287c454392668f18102a2d3c4baf4c7f7
Anukar <124155412+AnukarOP@users.noreply.github.com>
2023-11-04 11:42:56 +0530

    Delete src/src/components directory

--- src/src/components/Button.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <button
-    class="px-6 py-2 bg-blue-400 hover:bg-blue-500 transition-colors font-medium rounded">
-    <slot></slot>
-  </button>
-</template>
--- src/src/components/brand.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <RouterLink to="/" class="font-medium hover:text-blue-400 transition-colors"
-    >p2p Screensharing</RouterLink
-  >
-</template>
