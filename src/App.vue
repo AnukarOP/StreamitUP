5c3dabd325d27d8e8be932ad31d54e63eaf57a2a
Anukar <124155412+AnukarOP@users.noreply.github.com>
2023-11-04 12:03:39 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+
+function resize() {
+  document.body.style.width = `${window.innerWidth}px`
+  document.body.style.height = `${window.innerHeight}px`
+}
+
+resize()
+
+onMounted(() => {
+  window.addEventListener('resize', resize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resize)
+})
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
