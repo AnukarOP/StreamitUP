704242dfb1854fbe59dcbb3113767c797fd4241f
Anukar <124155412+AnukarOP@users.noreply.github.com>
2023-11-04 11:55:58 +0530

    Create WaitingModal.vue

---
+++ src/components/WaitingModal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+
+defineEmits<{
+  (e: 'cancel'): void
+}>()
+</script>
+
+<template>
+  <div class="text-center">
+    <h2 class="motion-safe:animate-bounce text-xl mt-6 mb-8">
+      {{ title }}
+    </h2>
+
+    <button
+      class="text-red-400 hover:text-red-500 transition-colors"
+      @click="$emit('cancel')">
+      Cancel 😲
+    </button>
+  </div>
+</template>
